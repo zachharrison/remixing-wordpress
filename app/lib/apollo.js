a9d158459e8a9ff5ff50ad63cc17a937085c61f3
zachharrison <zharrison100@gmail.com>
2023-08-13 11:26:32 -0700

    env

--- app/lib/apollo.js
+++ app/lib/apollo.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'URL HERE',
+  uri: process.env.GRAPHQL_ENDPOINT,
   cache: new InMemoryCache(),
 });
