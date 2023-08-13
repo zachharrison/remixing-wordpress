2eb357a78a91d9d3c67e0ece386d1257eefc7700
zachharrison <zharrison100@gmail.com>
2023-08-12 20:21:56 -0700

    update url

--- app/lib/apollo.js
+++ app/lib/apollo.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://fightinsights-net.preview-domain.com/graphql',
+  uri: 'URL HERE',
   cache: new InMemoryCache(),
 });
